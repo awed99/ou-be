c96b7ec16660426dd0c86c5fe3a922bd8ea014d8
Dewa Danu Brata <dewadanubrata@gmail.com>
2024-01-07 01:44:53 +0700

    fixing 3q

--- app/Controllers/Callbacks.php
+++ app/Controllers/Callbacks.php
@@ -137,16 +137,16 @@ class Callbacks extends BaseController
 
         if ($dt['status'] === '1' || $dt['status'] === 1) {
             $update['status'] = 'Waiting for SMS';
-            $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
+            // $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
         } else if ($dt['status'] === '3' || $dt['status'] === 3) {
             $update['status'] = 'Waiting for Resend SMS';
-            $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
+            // $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
         } else if ($dt['status'] === '6' || $dt['status'] === 6) {
             $update['status'] = 'Success';
             $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
         } else if ($dt['status'] === '8' || $dt['status'] === 8) {
             $update['status'] = 'Cancel';
-            $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
+            // $update['sms_text'] = '('.$dt['code'].') '.$dt['text'];
             $update['is_done'] = '1';
         }
 
